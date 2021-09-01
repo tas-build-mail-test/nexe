12ac71dba3ea6492aae13c6426ceff1cd0422c42
Sachin-Rungta <87764393+Sachin-Rungta@users.noreply.github.com>
2021-09-01 16:30:34 +0530

    Update options.spec.ts

--- test/options.spec.ts
+++ test/options.spec.ts
@@ -6,26 +6,26 @@ const ext = process.platform === 'win32' ? '.exe' : ''
 
 describe('options', () => {
   describe('cwd', () => {
-      // it('should use process.cwd() if nothing is provided', () => {
-      //   const options = normalizeOptions()
-      //   expect(options.cwd).to.equal(process.cwd())
-      // })
-      // it('should use the main module in a package directory (if not in a TTY)', () => {
-      //   const options = normalizeOptions()
-      //   if (!process.stdin.isTTY) {
-      //     expect(options.input).to.equal('[stdin]')
-      //   } else {
-      //     expect(options.input).to.equal(path.resolve('./index.js'))
-      //   }
-      // })
-      // it('should resolve relative paths for input', () => {
-      //   const options = normalizeOptions({ input: 'test/fixture/entry.js' })
-      //   expect(options.input).to.equal(path.resolve('./test/fixture/entry.js'))
-      // })
-      // it('should accept a module entry as input', () => {
-      //   const options = normalizeOptions({ input: 'test/fixture' })
-      //   expect(options.input).to.equal(path.resolve('./test/fixture/entry.js'))
-      // })
+      it('should use process.cwd() if nothing is provided', () => {
+        const options = normalizeOptions()
+        expect(options.cwd).to.equal(process.cwd())
+      })
+      it('should use the main module in a package directory (if not in a TTY)', () => {
+        const options = normalizeOptions()
+        if (!process.stdin.isTTY) {
+          expect(options.input).to.equal('[stdin]')
+        } else {
+          expect(options.input).to.equal(path.resolve('./index.js'))
+        }
+      })
+      it('should resolve relative paths for input', () => {
+        const options = normalizeOptions({ input: 'test/fixture/entry.js' })
+        expect(options.input).to.equal(path.resolve('./test/fixture/entry.js'))
+      })
+      it('should accept a module entry as input', () => {
+        const options = normalizeOptions({ input: 'test/fixture' })
+        expect(options.input).to.equal(path.resolve('./test/fixture/entry.js'))
+      })
       it('should resolve pathed options against cwd', () => {
         const cwd = path.join(process.cwd(), 'test/fixture')
         const options = normalizeOptions({
@@ -41,16 +41,16 @@ describe('options', () => {
   })
 
   describe('remote', () => {
-    // it('should use default remote', () => {
-    //   const options = normalizeOptions({})
-    //   expect(options.remote).to.equal('https://github.com/nexe/nexe/releases/download/v3.3.3/')
-    // })
-    // it('should append trailing slash to third-party remote if necessary', () => {
-    //   const options = normalizeOptions({
-    //     remote: 'https://sitejs.org/nexe'
-    //   })
-    //   expect(options.remote).to.equal('https://sitejs.org/nexe/')
-    // })
+    it('should use default remote', () => {
+      const options = normalizeOptions({})
+      expect(options.remote).to.equal('https://github.com/nexe/nexe/releases/download/v3.3.3/')
+    })
+    it('should append trailing slash to third-party remote if necessary', () => {
+      const options = normalizeOptions({
+        remote: 'https://sitejs.org/nexe'
+      })
+      expect(options.remote).to.equal('https://sitejs.org/nexe/')
+    })
     it('should not append trailing slash to third-party remote that already has one', () => {
       const options = normalizeOptions({
         remote: 'https://sitejs.org/nexe/'
@@ -60,18 +60,18 @@ describe('options', () => {
   })
 
   describe('output', () => {
-    // it('should work', () => {
-    //   const options = normalizeOptions({
-    //     output: './some-output'
-    //   })
-    //   expect(options.output).to.equal(path.resolve(`./some-output${ext}`))
-    // })
-    // it('should default to the input file name if not index', () => {
-    //   const options = normalizeOptions({
-    //     input: './test/fixture'
-    //   })
-    //   expect(options.output).to.equal(path.resolve(`./entry${ext}`))
-    // })
+    it('should work', () => {
+      const options = normalizeOptions({
+        output: './some-output'
+      })
+      expect(options.output).to.equal(path.resolve(`./some-output${ext}`))
+    })
+    it('should default to the input file name if not index', () => {
+      const options = normalizeOptions({
+        input: './test/fixture'
+      })
+      expect(options.output).to.equal(path.resolve(`./entry${ext}`))
+    })
     it('should default to the folder/project name if filename is index', () => {
       const options = normalizeOptions({ cwd: './test/fixture2' })
       expect(options.output).to.equal(path.resolve(`./test/fixture2/fixture2${ext}`))
